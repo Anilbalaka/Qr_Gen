7e39096557f5b380db305376b336b1e82b99d551
B.Anil <148098328+Anilbalaka@users.noreply.github.com>
2023-12-21 15:00:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+    .prompt([
+        {
+            message: "Enter your URL:",
+            name: "URL",
+        }
+    ])
+    .then((answers) => {
+        const url = answers.URL;
+        var qr_png = qr.image(url);
+        const loc = fs.createWriteStream("qr_image.png");
+        qr_png.pipe(loc);
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
